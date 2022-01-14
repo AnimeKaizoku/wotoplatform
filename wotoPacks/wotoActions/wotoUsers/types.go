da9a59081bc362ddc081e1a148350bdd538f6dce
aliwoto <aminnimaj@gmail.com>
2022-01-14 14:06:48 +0100

    Add new fields to RegisterUserData and LoginUserData structs
    Signed-off-by: aliwoto <aminnimaj@gmail.com>

--- wotoPacks/wotoActions/wotoUsers/types.go
+++ wotoPacks/wotoActions/wotoUsers/types.go
@@ -1,11 +1,15 @@
 package wotoUsers
 
 type RegisterUserData struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
+	Username  string `json:"username"`
+	Password  string `json:"password"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
 }
 
 type LoginUserData struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
+	Username   string `json:"username"`
+	Password   string `json:"password"`
+	AuthKey    string `json:"auth_key"`
+	AccessHash string `json:"access_hash"`
 }
