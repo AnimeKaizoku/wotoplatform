7e931ff2dd2b4b86e85444500f9dbb61e8d68db7
aliwoto <woto@kaizoku.cyou>
2022-05-10 19:12:03 +0200

    Implement RegisterMediaData.ToNewMediaData method in wotoMedia package.
    Signed-off-by: aliwoto <woto@kaizoku.cyou>

--- wotoPacks/wotoActions/wotoMedia/methods.go
+++ wotoPacks/wotoActions/wotoMedia/methods.go
@@ -3,5 +3,27 @@ package wotoMedia
 import mdb "wp-server/wotoPacks/database/mediaDatabase"
 
 func (d *RegisterMediaData) ToNewMediaData() *mdb.NewMediaData {
-	return nil
+	return &mdb.NewMediaData{
+		Genre:       d.Genre,
+		Company:     d.Company,
+		Author:      d.Author,
+		Episode:     d.Episode,
+		MediaType:   d.MediaType,
+		Title:       d.Title,
+		Duration:    d.Duration,
+		Artist:      d.Artist,
+		Album:       d.Album,
+		Year:        d.Year,
+		Cover:       d.Cover,
+		File:        d.File,
+		Thumbnail:   d.Thumbnail,
+		Lyrics:      d.Lyrics,
+		Lang:        d.Lang,
+		LangCode:    d.LangCode,
+		Region:      d.Region,
+		SourceUrl:   d.SourceUrl,
+		ExternalUrl: d.ExternalUrl,
+		IsPrivate:   d.IsPrivate,
+		Description: d.Description,
+	}
 }
